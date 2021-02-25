0e4f0f321e27b3330c035776d3c9da015f3f42c9
Akiacode <catry.me@gmail.com>
2021-02-26 00:19:02 +0900

    renew encode

--- src/decode.js
+++ src/decode.js
@@ -5,34 +5,7 @@ module.exports = function decode(code) {
     let d = []
 
     for (i of code) {
-        if (i.charCodeAt(0) === map[32]) { d.push(" "); continue }
-        if (i.charCodeAt(0) >= 44034 && i.charCodeAt(0) <= 44100) { d.push(String.fromCharCode(Number(findKey(i, map)))); continue  }
-
-        if (i.charCodeAt(0) > 11201) {
-            if (i.charCodeAt(0) < 50000) {
-                if (i.charCodeAt(0) < 40000) {
-                    d.push(String.fromCharCode(Number(findKey(i, map))+(11201)))
-                } else {
-                    if (i.charCodeAt(0) >= 44051 && i.charCodeAt(0) <= 44200) {
-                        d.push(String.fromCharCode(Number(findKey(i, map))))
-                    } else {
-                        if (i.charCodeAt(0) >= 45394 && i.charCodeAt(0) <= 45423) {
-                            d.push(String.fromCharCode(Number(findKey(i, map))+(11201)))
-                        } else {
-                            if (i.charCodeAt(0) >= 49702 && i.charCodeAt(0) <= 49800) {
-                                d.push(String.fromCharCode(Number(findKey(i, map))+(11201*4)))
-                            } else {
-                                d.push(String.fromCharCode(Number(findKey(i, map))+(11201*3)+10000))
-                            }
-                        }
-                    }
-                }
-            } else {
-                d.push(String.fromCharCode(Number(findKey(i, map))+(11201*4)))
-            }
-        } else {
-            d.push(String.fromCharCode(Number(findKey(i, map))))
-        }
+        //TODO
     }
 
     let text = ''
--- src/encode.js
+++ src/encode.js
@@ -5,18 +5,24 @@ module.exports = function encode(code) {
     let e = []
 
     for (i of code) {
-        if (i.charCodeAt(0) > 11201) {
-            if (i.charCodeAt(0) > 50000) {
-                e.push(String.fromCharCode(map[i.charCodeAt(0)-(11201*4)]))
+        if (i === '\n') { continue }
+
+        if  (map[i.charCodeAt(0)-(11201*4)] != undefined) {
+            e.push(String.fromCharCode(map[i.charCodeAt(0)-(11201*4)]))
+        } else {
+            if  (map[i.charCodeAt(0)-(11201*3)] != undefined) {
+                e.push(String.fromCharCode(map[i.charCodeAt(0)-(11201*3)]))
             } else {
-                if (i.charCodeAt(0) > 40000) {
-                    e.push(String.fromCharCode(map[i.charCodeAt(0)-(11201*3)-10000]))
+                if  (map[i.charCodeAt(0)-(11201)] != undefined) {
+                    e.push(String.fromCharCode(map[i.charCodeAt(0)-(10000)]) + '정')
+                    e.push(String.fromCharCode(map[i.charCodeAt(0)-(11201)]) + '님')
                 } else {
-                    e.push(String.fromCharCode(map[i.charCodeAt(0)-(11201)]))
+                    e.push(String.fromCharCode(map[i.charCodeAt(0)+(1111)]) + '바')
+                    e.push(String.fromCharCode(map[i.charCodeAt(0)+(2222)]) + '부')
+                    e.push(String.fromCharCode(map[i.charCodeAt(0)]))
+                    //console.log(i, i.charCodeAt(0), map[i.charCodeAt(0)])
                 }
             }
-        } else {
-            e.push(String.fromCharCode(map[i.charCodeAt(0)]))
         }
     }
 
